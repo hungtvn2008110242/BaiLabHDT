ce4cb2ae4959afb3c766acfd5a6984dc42e38036
hungtvn2008110242 <nguyenhung.gl.2212@gmail.com>
2021-10-19 18:54:13 +0700

    baitaptuan5

---
+++ src/com/hungtv2008110242/tuan5/car.java
@@ -0,0 +1,23 @@
+package com.hungtv2008110242.tuan5;
+
+import java.util.Scanner;
+
+public class car {
+    String tenChuXe, hangSanXuat, dong;
+    Boolean giayPhep;
+    float dungTichXang;
+    Scanner tenchuxe = new Scanner(System.in);
+    Scanner hangsx = new Scanner(System.in);
+    Scanner dongxe = new Scanner(System.in);
+    Scanner giayphep = new Scanner(System.in);
+    Scanner dungtichxang = new Scanner(System.in);
+
+    void inThongTinXe() {
+        System.out.println("Tên chủ xe : " + tenchuxe);
+        System.out.println("Hảng sản xuất : " + hangsx);
+        System.out.println("Dòng xe : " + dongxe);
+        System.out.println("Giấy phép : " + giayphep);
+        System.out.println("dung tích xăng : " + dungtichxang);
+
+    }
+}
